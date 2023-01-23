2f2dc94c23b5c3a161dcc83566f17be87f86cf71
Shokhzod Azamatov <ashotm1999@gmail.com>
2023-01-23 20:34:21 +0500

    written-product-repository

---
+++ src/Product.Api/Product.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Product.Api.Entities;
+
+namespace Product.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductModel>> GetAllProductAsync();
+        Task<ProductModel> GetProductAsync(Guid productId);
+        Task CreateProductAsync(ProductModel product);
+        Task DeleteProductAsync(Guid productId);
+        Task<ProductModel> UpdateProductAsync(Guid productId,ProductModel productModel);
+    }
+}
---
+++ src/Product.Api/Product.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Product.Api.Entities;
+
+namespace Product.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MongoClient _mongoClient;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<ProductModel> _products;
+
+        public ProductRepository()
+        {
+            _mongoClient = new MongoClient("mongodb://mongodb:27017");
+            _database = _mongoClient.GetDatabase("products_db");
+            _products = _database.GetCollection<ProductModel>("products");
+        }
+        public async Task CreateProductAsync(ProductModel product)
+        {
+            product.Id = Guid.Parse(ObjectId.GenerateNewId(DateTime.Now).ToString());
+
+            await _products.InsertOneAsync(product);
+        }
+
+        public async Task DeleteProductAsync(Guid productId)
+        {
+            var result = await _products.DeleteOneAsync(e => e.Id == productId);
+            if (result.DeletedCount == 0)
+                throw new Exception();
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAllProductAsync()
+        {
+            var products = await(await _products.FindAsync(product => true)).ToListAsync();
+
+            return products;
+        }
+
+        public async Task<ProductModel> GetProductAsync(Guid productId)
+        {
+            var product = await (await _products.FindAsync(product => product.Id==productId)).SingleOrDefaultAsync();
+            if(product is null)
+                throw new Exception();
+            return product;
+        }
+
+        public async Task<ProductModel> UpdateProductAsync(Guid productId, ProductModel productModel)
+        {
+            var product = await (await _products.FindAsync(product => product.Id == productId)).SingleOrDefaultAsync();
+            product.Id = productId;
+            await _products.ReplaceOneAsync(e => e.Id == productId,productModel, new ReplaceOptions { IsUpsert = true });
+            return product;
+        }
+    }
+}
