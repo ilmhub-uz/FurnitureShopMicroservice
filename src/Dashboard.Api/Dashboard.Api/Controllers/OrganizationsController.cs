72719e36144ab69f8aadbb985f6d6987c76ba136
ext1z <ext1z1tabgod@gmail.com>
2023-01-23 21:07:52 +0500

    db contexts

--- src/Dashboard.Api/Dashboard.Api/Controllers/OrganizationsController.cs
+++ src/Dashboard.Api/Dashboard.Api/Controllers/OrganizationsController.cs
@@ -4,44 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Dashboard.Api.Controllers
+
+
+namespace Dashboard.Api.Controllers;
+
+[Route("api/[controller]")]
+public class OrganizationsController : Controller
 {
-    [Route("api/[controller]")]
-    public class OrganizationsController : Controller
+
+    [HttpGet]
+    public async Task<IActionResult> GetOrganizations()
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        return Ok();
+    }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    [HttpGet("{organization:Guid}")]
+    public async Task<IActionResult> GetOrganizationById(Guid organizationId)
+    {
+        return Ok();
+    }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    [HttpPut("{organization:Guid}")]
+    public async Task<IActionResult> OrganizationUpdateStatus(Guid organizationId)
+    {
+        return Ok();
     }
 }
 
