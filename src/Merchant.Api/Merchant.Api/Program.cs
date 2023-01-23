5e92f24001d98e9af23065c43f98f6696363b634
AbdulazizCoding <abdulazizabduqaxxarov04@gmail.com>
2023-01-24 01:46:24 +0500

    Added Migrations in Merchant api

--- src/Merchant.Api/Merchant.Api/Program.cs
+++ src/Merchant.Api/Merchant.Api/Program.cs
@@ -4,6 +4,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
