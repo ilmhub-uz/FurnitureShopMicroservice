df7735f014c2919393c0b78c5a9969f614d2b817
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-23 21:26:35 +0500

    added docker-compose for Cart.Api

---
+++ src/Cart.Api/Cart.Api/Controllers/CartsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Cart.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CartsController : ControllerBase
+{
+    private readonly IDistributedCache _distributedCache;
+
+    public CartsController(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddProduct()
+    {
+         await _distributedCache.SetStringAsync("key", "value");
+
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCart()
+    {
+        return Ok(await _distributedCache.GetStringAsync("key"));
+    }
+}
