d1fa56ce5cf5a9371b39a595d625661385e73e17
ext1z <ext1z1tabgod@gmail.com>
2023-01-24 15:07:29 +0500

    errors fixed

--- src/Dashboard.Api/Dashboard.Api/Services/OrganizationService.cs
+++ src/Dashboard.Api/Dashboard.Api/Services/OrganizationService.cs
@@ -49,11 +49,13 @@ public class OrganizationService : IOrganizationService
         if (organization is null)
         {
             throw new Exception("not found");
+
+        }
+
         organization.Name = updateOrganizationDto.Name;
         organization.Status = updateOrganizationDto.Status;
 
         await _context.SaveChangesAsync();
+
     }
-
-
 }
