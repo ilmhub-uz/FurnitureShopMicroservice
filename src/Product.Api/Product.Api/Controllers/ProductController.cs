d12677450675c0a68d0ca708a2ebe9ffa68bfa86
Jamoliddin <shermatovjamoliddin3@gmail.com>
2023-01-24 15:20:18 +0500

    added-rabbitmq

--- src/Product.Api/Product.Api/Controllers/ProductController.cs
+++ src/Product.Api/Product.Api/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Product.Api.Entities.Dtos;
+using Product.Api.Entities.ViewModels;
 using Product.Api.Repositories;
+using System.Collections.Generic;
 
 namespace Product.Api.Controllers
 {
@@ -23,10 +25,12 @@ namespace Product.Api.Controllers
 		}
 
 		[HttpGet]
+		[ProducesResponseType(typeof(List<ProductViewModel>),StatusCodes.Status200OK)]
 		public async Task<IActionResult> GetProduct()
 		=> Ok(await repository.GetAllProductAsync());
 
 		[HttpGet("getbyId")]
+		[ProducesResponseType(typeof(ProductViewModel),StatusCodes.Status200OK)]
 		public async Task<IActionResult> GetProductById(string productId)
 			=> Ok(await repository.GetProductAsync(productId));
 
---
+++ src/Product.Api/Product.Api/RabbitMq/SendToGetMessage.cs
@@ -0,0 +1,35 @@
+﻿using Product.Api.Entities;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Product.Api.RabbitMq
+{
+	public class SendToGetMessage
+	{
+		public void SendMessage(ProductModel product)
+		{
+			var factory = new ConnectionFactory
+			{
+				HostName = "furniture_rabbitmq",
+				UserName = "guest",
+				Password = "guest",
+				Port = 5672
+			};
+
+			var connection = factory.CreateConnection();
+			var channel = connection.CreateModel();
+
+			//channel.QueueDeclare("product_added", false, false, false, null);
+
+			channel.ExchangeDeclare("product_added", ExchangeType.Fanout);
+
+			var productJson = Newtonsoft.Json.JsonConvert.SerializeObject(product);
+			var productJsonByte = Encoding.UTF8.GetBytes(productJson);
+
+			channel.BasicPublish("product_added", "", null, productJsonByte);
+
+			channel.Close();
+			connection.Close();
+		}
+	}
+}
