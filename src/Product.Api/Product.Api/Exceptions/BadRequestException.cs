5117b9fbb23da02a24c3dd5cc3fefe75a07368ec
Jamoliddin <shermatovjamoliddin3@gmail.com>
2023-01-24 13:35:26 +0500

    added-exception/middlewere

---
+++ src/Product.Api/Product.Api/Exceptions/BadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace Product.Api.Exceptions
+{
+	public class BadRequestException : Exception
+	{
+		public int ErrorCode { get; set; }
+		public BadRequestException(string message) : base(message) { }
+	}
+}
