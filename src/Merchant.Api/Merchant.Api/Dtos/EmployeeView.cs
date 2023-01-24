360bd954fa80fd372da7dc192c5330ed98f1fcdf
NiyatovAziz <niyatovazizbek737.com>
2023-01-24 16:56:09 +0500

    MerchantApi/Employee-added

---
+++ src/Merchant.Api/Merchant.Api/Dtos/EmployeeView.cs
@@ -0,0 +1,10 @@
+﻿using Merchant.Api.Entities;
+
+namespace Merchant.Api.Dtos;
+
+public class EmployeeView
+{
+    public Guid Id { get; set;}
+    public string? Name { get; set;}
+    public ERole Role { get; set;}
+}
---
+++ src/Merchant.Api/Merchant.Api/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using Merchant.Api.Dtos;
+using System.Security.Claims;
+
+namespace Merchant.Api.Services;
+
+public interface IEmployeeService
+{
+    Task CreateEmployeeAsync(CreateEmployeeDto createEmployee);
+    Task<List<EmployeeView>?> GetEmployeesAsync(Guid organizationId);
+    Task<EmployeeView> GetEmployeeByIdAsync(Guid OrganizationId,Guid employeeId);
+    Task UpdatEmployeeAsync(Guid organizationId,Guid employeeId, UpdateEmployeeDto updateEmployee);
+    Task DeleteEmployeeAsync(Guid OrganizationId,Guid employeeId);
+}
