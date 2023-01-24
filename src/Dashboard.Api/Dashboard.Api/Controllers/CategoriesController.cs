6feaf733fb66b7b6790785784b3d589b21ad6760
Ubaydulayev <ubaydullayevdastonbek@gmail.com>
2023-01-24 20:52:52 +0500

    doston category controller

--- src/Dashboard.Api/Dashboard.Api/Controllers/CategoriesController.cs
+++ src/Dashboard.Api/Dashboard.Api/Controllers/CategoriesController.cs
@@ -1,37 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Dashboard.Api.ModelsDto;
+using Dashboard.Api.Services.Interfaces;
+using Dashboard.Api.ViewModels;
 using JFA.DependencyInjection;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Dashboard.Api.Controllers;
 [Route("api/[controller]")]
+[Scoped]
 public class CategoriesController : Controller
 {
+    private readonly UpdateCategoryDto _updateCategoryDto;
+    private readonly ICategoriesService _categoriesService;
+
+    public CategoriesController(UpdateCategoryDto updateCategoryDto, ICategoriesService categoriesService)
+    {
+        _categoriesService = categoriesService;
+        _updateCategoryDto = updateCategoryDto;
+    }
+    
     [HttpGet]
+    [ProducesResponseType(typeof(List<CategoryView>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetCategories()
     {
+        var category = await _categoriesService.GetCategories();
+        return Ok(category);
+    }
+
+    [HttpGet("{categoryId:guid}")]
+    [ProducesResponseType(typeof(CategoryView), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetCategoryById(Guid categoryId)
+    {
+        await _categoriesService.GetCategoryByIdAsync(categoryId);
         return Ok();
     }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateCategories()
+    [HttpPut("{categoryId:guid}")]
+    public async Task<IActionResult> UpdateCategories(Guid categoryId, UpdateCategoryDto updateCategoryDto)
     {
-        return Ok();
-    }
-
-    [HttpPut]
-    public async Task<IActionResult> UpdateCategories()
-    {
-        return Ok();
-    }
-
-    [HttpDelete]
-    public async Task<IActionResult> DaleteCategories()
-    {
+        await _categoriesService.UpdateCategoriesStatus(updateCategoryDto, categoryId);
         return Ok();
     }
 }
