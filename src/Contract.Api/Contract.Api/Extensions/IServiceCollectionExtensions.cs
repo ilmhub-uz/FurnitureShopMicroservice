b2305cffdb1577ba495fc0b98ba0b289dfd44c30
Maxammatov_Abdurauf <maxammatovabdurauftdyu@gmail.com>
2023-01-24 19:31:19 +0500

    I have added extension file to just check

---
+++ src/Contract.Api/Contract.Api/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Contract.Api.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddLogger (this IServiceCollection services)
+    {
+        services.AddCors(option =>
+        {
+            option.AddPolicy("base",options =>
+            {
+                options.AllowAnyHeader().AllowAnyOrigin();
+            });
+        });
+    }
+}
