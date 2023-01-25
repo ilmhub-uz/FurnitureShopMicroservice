e8666b2700732a05327bf3845fabb430b76e4118
AbdulazizCoding <abdulazizabduqaxxarov04@gmail.com>
2023-01-25 20:11:39 +0500

    Added RabbitMqConfiguration in Merchant API

---
+++ src/Merchant.Api/Merchant.Api/Configurations/RabbitMqConfigurations.cs
@@ -0,0 +1,12 @@
+﻿namespace Merchant.Api.Configurations;
+
+public class RabbitMqConfigurations
+{
+    public string? HostName { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public int Port { get; set; }
+    public string? PostOrganizationName { get; set; }
+    public string? PostEmploeeName { get; set; }
+    public string? GetProductName { get; set; }
+}
--- src/Merchant.Api/Merchant.Api/Program.cs
+++ src/Merchant.Api/Merchant.Api/Program.cs
@@ -1,12 +1,17 @@
+using Merchant.Api.Configurations;
 using Merchant.Api.Data;
 using Merchant.Api.Middleware;
 using Merchant.Api.Repositories;
 using Merchant.Api.Services;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var rabbitMqConfig = builder.Configuration
+    .GetSection("RabbitMqConfigurations")
+    .Get<RabbitMqConfigurations>();
+
+builder.Services.AddSingleton(rabbitMqConfig);
 
 builder.Services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +24,7 @@ builder.Services.AddHostedService<ProductAddService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseLazyLoadingProxies().UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
